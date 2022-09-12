d19f019ce94c3a007cd00806e761f1307681054c
Vladimir Babenko <vvbabenko87@yandex.ru>
2022-09-12 20:43:36 +0300

    added style for main-navigation--nojs, added script nojs

--- source/js/index.js
+++ source/js/index.js
@@ -1,8 +1,10 @@
 // closed and opened navigation menu
 const toggle = document.querySelector('.main-navigation__toggle');
-const mainNav = document.querySelector('.main-navigation')
+const mainNav = document.querySelector('.main-navigation');
+
+mainNav.classList.remove('main-navigation--nojs');
 
 toggle.addEventListener('click', ()=> {
-  mainNav.classList.toggle('main-navigation--opened')
-  mainNav.classList.toggle('main-navigation--closed')
+  mainNav.classList.toggle('main-navigation--opened');
+  mainNav.classList.toggle('main-navigation--closed');
 })
